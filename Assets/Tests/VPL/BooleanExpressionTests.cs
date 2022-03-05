cf7f895e489bc8912d7fec183958d65eb917d3ef
eamansour <eamonn.mansour@gmail.com>
2022-03-05 23:19:27 +0000

    Add logical operator tests

--- Assets/Tests/VPL/BooleanExpressionTests.cs
+++ Assets/Tests/VPL/BooleanExpressionTests.cs
@@ -17,38 +17,60 @@ public class BooleanExpressionTests
     private TMP_Dropdown _operatorDropdown;
     private TMP_Dropdown _logicalDropdown;
 
+    // Fields for logical operator tests
+    private TMP_InputField _leftOperandNew;
+    private TMP_InputField _rightOperandNew;
+    private TMP_Dropdown _operatorDropdownNew;
+    private TMP_Dropdown _logicalDropdownNew;
+
     [SetUp]
     public void SetUp()
     {
         _testObject = new GameObject();
-        _testObject.AddComponent<Image>();
 
-        _boolExpression = _testObject.AddComponent<BooleanExpression>();
-        
-        _operatorDropdown = new GameObject().AddComponent<TMP_Dropdown>();
-        _logicalDropdown = new GameObject().AddComponent<TMP_Dropdown>();
+        GameObject expressionObject = new GameObject("TestExpression");
+        expressionObject.AddComponent<Image>();
+        _boolExpression = expressionObject.AddComponent<BooleanExpression>();
+        _boolExpression.transform.SetParent(_testObject.transform);
 
-        _leftOperand = new GameObject().AddComponent<TMP_InputField>();
-        _rightOperand = new GameObject().AddComponent<TMP_InputField>();
+        List<string> operatorList = new List<string> { "==", "!=", "<", ">", "<=", ">=" };
+        List<string> logicalOperatorList = new List<string> { "...", "and", "or" };
 
-        BooleanExpression prefab =
-            Resources.Load<BooleanExpression>("Prefabs/VPL/Expressions/BooleanExpression");
+        _operatorDropdown = CreateTestDropdown(operatorList);
+        _logicalDropdown = CreateTestDropdown(logicalOperatorList);
+        _operatorDropdownNew = CreateTestDropdown(operatorList);
+        _logicalDropdownNew = CreateTestDropdown(logicalOperatorList);
 
-        _operatorDropdown.AddOptions(new List<string> { "==", "!=", "<", ">", "<=", ">=" });
-        _logicalDropdown.AddOptions(new List<string> { "...", "and", "or" });
+        _leftOperand = CreateTestInputField();
+        _rightOperand = CreateTestInputField();
+        _leftOperandNew = CreateTestInputField();
+        _rightOperandNew = CreateTestInputField();
 
         _boolExpression.Construct(_leftOperand, _rightOperand, _operatorDropdown);
-        _boolExpression.Construct(_logicalDropdown, prefab);
+        _boolExpression.Construct(_logicalDropdown, _boolExpression);
     }
 
     [TearDown]
     public void TearDown()
     {
-        foreach (GameObject go in GameObject.FindObjectsOfType<GameObject>())
-        {
-            go.SetActive(true);
-            Object.Destroy(go);
-        }
+        Object.Destroy(_testObject);
+    }
+
+    // Creates a new input field and sets its parent to the testObject
+    private TMP_InputField CreateTestInputField()
+    {
+        TMP_InputField inputField = new GameObject().AddComponent<TMP_InputField>();
+        inputField.transform.SetParent(_testObject.transform);
+        return inputField;
+    }
+
+    // Creates a new dropdown with a given list of options and sets its parent to the testObject
+    private TMP_Dropdown CreateTestDropdown(List<string> options)
+    {
+        TMP_Dropdown dropdown = new GameObject().AddComponent<TMP_Dropdown>();
+        dropdown.transform.SetParent(_testObject.transform);
+        dropdown.AddOptions(options);
+        return dropdown;
     }
 
     [Test]
@@ -179,22 +201,128 @@ public class BooleanExpressionTests
     }
 
     [UnityTest]
-    public IEnumerator LogicalAnd_ShouldReturnTrueIfBothExpressionsAreTrue(
-        [Values("5")] string left,
-        [Values("5")] string right
-    )
+    public IEnumerator LogicalAnd_ShouldReturnTrueIfBothExpressionsAreTrue()
     {
         _operatorDropdown.value = 0;
         _logicalDropdown.value = 1;
-        _leftOperand.text = left;
-        _rightOperand.text = right;
+        _leftOperand.text = "5";
+        _rightOperand.text = "5";
+
+        _operatorDropdownNew.value = 0;
+        _logicalDropdownNew.value = 0;
+        _leftOperandNew.text = "-2.5";
+        _rightOperandNew.text = "-2.5";
 
         yield return new WaitForSeconds(0.1f);
-        BooleanExpression newExpression = GameObject.Find("BooleanExpression(Clone)").GetComponent<BooleanExpression>();
-        newExpression.Construct(_leftOperand, _rightOperand, _operatorDropdown);
-        
-        bool result = _boolExpression.EvaluateExpression();
 
+        BooleanExpression newExpression = GameObject.Find("TestExpression(Clone)").GetComponent<BooleanExpression>();
+        newExpression.Construct(_leftOperandNew, _rightOperandNew, _operatorDropdownNew);
+        newExpression.Construct(_logicalDropdownNew, _boolExpression);
+
+        bool result = _boolExpression.EvaluateExpression();
         Assert.IsTrue(result);
+    }
+
+    [UnityTest]
+    public IEnumerator LogicalAnd_ShouldReturnFalseIfAnExpressionIsFalse()
+    {
+        // Change logical operator to "and"
+        _logicalDropdown.value = 1;
+
+        _operatorDropdown.value = 0;
+        _leftOperand.text = "5";
+        _rightOperand.text = "5";
+
+        _operatorDropdownNew.value = 0;
+        _logicalDropdownNew.value = 0;
+        _leftOperandNew.text = "1.5";
+        _rightOperandNew.text = "2.5";
+
+        yield return new WaitForSeconds(0.1f);
+
+        BooleanExpression newExpression = GameObject.Find("TestExpression(Clone)").GetComponent<BooleanExpression>();
+        newExpression.Construct(_leftOperandNew, _rightOperandNew, _operatorDropdownNew);
+        newExpression.Construct(_logicalDropdownNew, _boolExpression);
+
+        bool result = _boolExpression.EvaluateExpression();
+        Assert.IsFalse(result);
+    }
+
+    [UnityTest]
+    public IEnumerator LogicalOr_ShouldReturnTrueIfAtLeastOneExpressionIsTrue()
+    {
+        // Change logical operator to "or"
+        _logicalDropdown.value = 2;
+
+        _operatorDropdown.value = 0;
+        _leftOperand.text = "5";
+        _rightOperand.text = "5";
+
+        _operatorDropdownNew.value = 0;
+        _logicalDropdownNew.value = 0;
+        _leftOperandNew.text = "0";
+        _rightOperandNew.text = "2";
+
+        yield return new WaitForSeconds(0.1f);
+
+        BooleanExpression newExpression = GameObject.Find("TestExpression(Clone)").GetComponent<BooleanExpression>();
+        newExpression.Construct(_leftOperandNew, _rightOperandNew, _operatorDropdownNew);
+        newExpression.Construct(_logicalDropdownNew, _boolExpression);
+
+        bool result = _boolExpression.EvaluateExpression();
+        Assert.IsTrue(result);
+    }
+
+    [UnityTest]
+    public IEnumerator LogicalOr_ShouldReturnFalseIfBothExpressionsAreFalse()
+    {
+        // Change logical operator to "or"
+        _logicalDropdown.value = 2;
+
+        _operatorDropdown.value = 0;
+        _leftOperand.text = "1";
+        _rightOperand.text = "5";
+
+        _operatorDropdownNew.value = 0;
+        _logicalDropdownNew.value = 0;
+        _leftOperandNew.text = "0";
+        _rightOperandNew.text = "2";
+
+        yield return new WaitForSeconds(0.1f);
+
+        BooleanExpression newExpression = GameObject.Find("TestExpression(Clone)").GetComponent<BooleanExpression>();
+        newExpression.Construct(_leftOperandNew, _rightOperandNew, _operatorDropdownNew);
+        newExpression.Construct(_logicalDropdownNew, _boolExpression);
+
+        bool result = _boolExpression.EvaluateExpression();
+        Assert.IsFalse(result);
+    }
+
+    [UnityTest]
+    public IEnumerator NoLogicalOperator_ShouldDeleteNewExpression()
+    {
+        // Change logical operator to "and"
+        _logicalDropdown.value = 1;
+
+        _operatorDropdown.value = 0;
+        _leftOperand.text = "1";
+        _rightOperand.text = "5";
+
+        _operatorDropdownNew.value = 0;
+        _logicalDropdownNew.value = 0;
+        _leftOperandNew.text = "0";
+        _rightOperandNew.text = "2";
+
+        yield return new WaitForSeconds(0.1f);
+
+        BooleanExpression newExpression = GameObject.Find("TestExpression(Clone)").GetComponent<BooleanExpression>();
+        Assert.IsNotNull(newExpression);
+
+        // Change logical operator to none (default "..." option)
+        _logicalDropdown.value = 0;
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.IsTrue(newExpression == null);
     }
 }
